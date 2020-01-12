5ba00b16f0d2ee348fc369fa514a02d695b42f76
Apoorve Shrivastava <apoorve@gmail.com>
2020-01-12 02:20:33 -0600

    Zipkin setup for distributed tracing.

---
+++ zipkin/src/test/java/com/mssample/zipkin/ZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mssample.zipkin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZipkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
